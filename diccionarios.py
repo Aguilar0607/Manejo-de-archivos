879a17942c62d31fbaed1829afad1f0fb175deac
Aguilar0607 <emmanuelaguilar0607@gmail.com>
2022-11-15 16:05:15 +0000

    Manejo de archivos

---
+++ diccionarios.py
@@ -0,0 +1,24 @@
+alumno = {
+        "name": "Emmanuel",
+        "email": "emmanuelaguilar0607@gmail.com",
+        "age": 20,
+        "series_favoritas": ['MrRobot'. 'Scorpion']
+        }
+json =  [{
+        "name": "Emmanuel",
+        "email": "emmanuelaguilar0607@gmail.com",
+        "age": 20,
+        "series_favoritas": ['MrRobot'. 'Scorpion']
+        },
+        {
+        "name": "Emmanuel",
+        "email": "emmanuelaguilar0607@gmail.com",
+        "age": 20,
+        "series_favoritas": ['MrRobot'. 'Scorpion']
+        },
+        {
+        "name": "Emmanuel",
+        "email": "emmanuelaguilar0607@gmail.com",
+        "age": 20,
+        "series_favoritas": ['MrRobot'. 'Scorpion']
+        }]
